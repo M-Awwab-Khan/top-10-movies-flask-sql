765bb000f15cc7d2b80ba25ff20addab9683a0f2
Awwab <mawwabkhank2006@gmail.com>
2024-02-15 19:22:17 +0500

    initial database setup

--- main.py
+++ main.py
@@ -8,27 +8,32 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Movies.db"
 Bootstrap5(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
 
+db = SQLAlchemy(model_class=Base)
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, primary_key=True)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 # CREATE TABLE
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
