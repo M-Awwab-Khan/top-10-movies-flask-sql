158d28a9d2fda4a5efae3a3cf9de64b2df584952
Awwab <mawwabkhank2006@gmail.com>
2024-02-15 19:32:46 +0500

    primary key bug fixed

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ db = SQLAlchemy(model_class=Base)
 class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    year: Mapped[int] = mapped_column(Integer, primary_key=True)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
     description: Mapped[str] = mapped_column(String(250), nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=False)
     ranking: Mapped[int] = mapped_column(Integer, nullable=False)
